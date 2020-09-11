ddc287b4e91830ec313931139cbd687f1070f9cf
albin <albinmonsimier@gmail.com>
2020-09-11 17:51:16 +0200

    green ground

--- src/scenes/scene/index.ts
+++ src/scenes/scene/index.ts
@@ -11,6 +11,7 @@ import { attachScripts, configurePostProcesses } from "../tools";
 export const scriptsMap = {
 	"src/scenes/scene/camera.ts": require("./camera"),
 	"src/scenes/scene/fallingElement.ts": require("./fallingElement"),
+	"src/scenes/scene/hud.ts": require("./hud"),
 	"src/scenes/scene/sun.ts": require("./sun"),
 }
 
